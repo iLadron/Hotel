535600a1e5773fe0cba25f112448f5d4f4a11e3c
Дмитрий Гриник <dima.griniky@gmail.com>
2021-04-16 00:13:21 +0300

    fix

--- front/js/script.js
+++ front/js/script.js
@@ -39,22 +39,22 @@ $(document).ready(function(){
 	function setRating(rating) {
 	  $('#rating-input-more').val(rating);
 	  // fill all the stars assigning the '.selected' class
-	  $('.rating-star').removeClass('fa-star-o').addClass('selected');
+	  $('.rating-star-more').removeClass('fa-star-o').addClass('selected');
 	  // empty all the stars to the right of the mouse
-	  $('.rating-star#rating-more-' + rating + ' ~ .rating-star').removeClass('selected').addClass('fa-star-o');
+	  $('.rating-star-more#rating-more-' + rating + ' ~ .rating-star-more').removeClass('selected').addClass('fa-star-o');
 	}
 	
-	$('.rating-star')
+	$('.rating-star-more')
 	.on('mouseover', function(e) {
 	  var rating = $(e.target).data('rating');
 	  // fill all the stars
-	  $('.rating-star').removeClass('fa-star-o').addClass('fa-star');
+	  $('.rating-star-more').removeClass('fa-star-o').addClass('fa-star');
 	  // empty all the stars to the right of the mouse
-	  $('.rating-star#rating-more-' + rating + ' ~ .rating-star').removeClass('fa-star').addClass('fa-star-o');
+	  $('.rating-star-more#rating-more-' + rating + ' ~ .rating-star-more').removeClass('fa-star').addClass('fa-star-o');
 	})
 	.on('mouseleave', function (e) {
 	  // empty all the stars except those with class .selected
-	  $('.rating-star').removeClass('fa-star').addClass('fa-star-o');
+	  $('.rating-star-more').removeClass('fa-star').addClass('fa-star-o');
 	})
 	.on('click', function(e) {
 	  var rating = $(e.target).data('rating');
